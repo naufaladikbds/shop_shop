2fc6d28b67f5f21eeae3a79195dc2eb3b4f2415a
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-15 15:15:11 +0700

    Create custom widget (icon with notification badge)

---
+++ lib/widgets/icon_with_badge.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class IconWithBadge extends StatelessWidget {
+  final int badgeCount;
+  final void Function() onPress;
+  final Icon icon;
+
+  const IconWithBadge({
+    required this.badgeCount,
+    required this.onPress,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: onPress,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(12.0),
+            child: icon,
+          ),
+          Positioned(
+            top: 8,
+            right: 1,
+            child: ClipOval(
+              child: Container(
+                alignment: Alignment.center,
+                height: 20,
+                width: 20,
+                color: Colors.red,
+                child: Text(
+                  badgeCount.toString(),
+                  style: TextStyle(color: Colors.white, fontSize: 10),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
