02668a75c411bd3752952d840619e4eb2a9ab053
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-16 13:14:30 +0700

    Add product_detail_screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_shop/models/product.dart';
+import 'package:shop_shop/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  const ProductDetailScreen({Key? key}) : super(key: key);
+  static const String routeName = "/product-detail";
 
   @override
   Widget build(BuildContext context) {
+    // String id = ModalRoute.of(context)?.settings.arguments as String;
+    String id = 'p3';
+    Product product = Provider.of<ProductsProvider>(context, listen: false)
+        .searchItemById(id);
+
     return Scaffold(
-      appBar: AppBar(title: Text("")),
+      appBar: AppBar(title: Text(product.id)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 290,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: Text(
+                '${product.price}',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: Text('${product.description}'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
