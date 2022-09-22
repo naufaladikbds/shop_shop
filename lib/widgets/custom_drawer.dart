7a4c1220f3df6656e9b9dbbd8112e6ae1756df2d
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-22 08:27:51 +0700

    Add new navigation on custom_drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:shop_shop/screens/manage_product_screen.dart';
 import 'package:shop_shop/screens/orders_screen.dart';
 import 'package:shop_shop/screens/products_overview_screen.dart';
 
@@ -32,6 +33,15 @@ class CustomDrawer extends StatelessWidget {
             onTap: () {
               Navigator.pop(context); // close drawer
               Navigator.pushReplacementNamed(context, OrdersScreen.routeName);
+            },
+          ),
+          ListTile(
+            // tileColor: Colors.amber,
+            title: Text('Manage Product'),
+            onTap: () {
+              Navigator.pop(context); // close drawer
+              Navigator.pushReplacementNamed(
+                  context, ManageProductScreen.routeName);
             },
           ),
         ],
