f7911e73ef60c7371e0afdc891c372c680f4cfac
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-23 10:36:19 +0700

    Add focusNode for better UX. Add maxlines to description

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -36,6 +36,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final descCtrl = TextEditingController();
   final imageCtrl = TextEditingController();
 
+  final priceFocus = FocusNode();
+  final descFocus = FocusNode();
+  final imageUrlFocus = FocusNode();
+
   @override
   void initState() {
     if (widget.productId != null) {
@@ -45,6 +49,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
       imageCtrl.text = widget.imageUrl;
     }
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    priceFocus.dispose();
+    descFocus.dispose();
+    priceFocus.dispose();
+    super.dispose();
   }
 
   @override
@@ -58,129 +70,144 @@ class _EditProductScreenState extends State<EditProductScreen> {
         borderRadius: BorderRadius.circular(10),
         color: Colors.grey[100],
       ),
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 40,
-              vertical: 15,
-            ),
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  labelText: 'Title',
-                  controller: titleCtrl,
-                ),
-                CustomTextFormField(
-                  labelText: 'Price',
-                  controller: priceCtrl,
-                ),
-                CustomTextFormField(
-                  labelText: 'Description',
-                  controller: descCtrl,
-                ),
-                Container(
-                  decoration: BoxDecoration(),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CustomTextFormField(
-                        labelText: 'Image URL',
-                        withImagePreview: true,
-                        controller: imageCtrl,
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.preview),
-                          onPressed: () {
-                            setState(() {
-                              displayPreview = true;
-                            });
-                          },
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 15,
+              ),
+              child: Form(
+                autovalidateMode: AutovalidateMode.always,
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      labelText: 'Title',
+                      controller: titleCtrl,
+                      onFieldSubmitted: (_) =>
+                          FocusScope.of(context).requestFocus(priceFocus),
+                    ),
+                    CustomTextFormField(
+                      labelText: 'Price',
+                      controller: priceCtrl,
+                      focusNode: priceFocus,
+                      onFieldSubmitted: (_) =>
+                          FocusScope.of(context).requestFocus(descFocus),
+                    ),
+                    CustomTextFormField(
+                      labelText: 'Description',
+                      controller: descCtrl,
+                      focusNode: descFocus,
+                      maxLines: 5,
+                      onFieldSubmitted: (_) =>
+                          FocusScope.of(context).requestFocus(imageUrlFocus),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CustomTextFormField(
+                            labelText: 'Image URL',
+                            withImagePreview: true,
+                            controller: imageCtrl,
+                            suffixIcon: IconButton(
+                              icon: Icon(Icons.preview),
+                              onPressed: () {
+                                setState(() {
+                                  displayPreview = true;
+                                });
+                              },
+                            ),
                           ),
-                          color: Colors.grey[300],
-                        ),
-                        width: double.infinity,
-                        child: !displayPreview && widget.productId == null
-                            ? Center(
-                                child: Text(
-                                  'Image preview here\n',
-                                  style: TextStyle(color: Colors.grey[600]),
-                                ),
-                              )
-                            : ClipRRect(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(10),
-                                  bottomRight: Radius.circular(10),
-                                ),
-                                child: Image.network(
-                                  imageCtrl.text,
-                                  fit: BoxFit.cover,
-                                  errorBuilder: (context, error, stackTrace) {
-                                    return Center(
-                                      child: Text(
-                                        'Please input a valid URL\n',
-                                        style: TextStyle(
-                                          color: Colors.grey[600],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
+                          Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
                               ),
+                              color: Colors.grey[300],
+                            ),
+                            width: double.infinity,
+                            child: !displayPreview && widget.productId == null
+                                ? Center(
+                                    child: Text(
+                                      'Image preview here\n',
+                                      style: TextStyle(color: Colors.grey[600]),
+                                    ),
+                                  )
+                                : ClipRRect(
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10),
+                                      bottomRight: Radius.circular(10),
+                                    ),
+                                    child: Image.network(
+                                      imageCtrl.text,
+                                      fit: BoxFit.cover,
+                                      errorBuilder:
+                                          (context, error, stackTrace) {
+                                        return Center(
+                                          child: Text(
+                                            'Please input a valid URL\n',
+                                            style: TextStyle(
+                                              color: Colors.grey[600],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    widget.productId != null
+                        ? productProvider.editProduct(
+                            widget.productId!,
+                            titleCtrl.text,
+                            double.parse(priceCtrl.text),
+                            descCtrl.text,
+                            imageCtrl.text,
+                          )
+                        : productProvider.addProduct(
+                            Product(
+                              id: DateTime.now().toString(),
+                              title: titleCtrl.text,
+                              description: descCtrl.text,
+                              price: double.parse(priceCtrl.text),
+                              imageUrl: imageCtrl.text,
+                            ),
+                          );
+
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    widget.productId == null ? 'Add Product' : 'Apply Changes',
                   ),
                 ),
               ],
-            ),
-          ),
-          SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Cancel'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  widget.productId != null
-                      ? productProvider.editProduct(
-                          widget.productId!,
-                          titleCtrl.text,
-                          double.parse(priceCtrl.text),
-                          descCtrl.text,
-                          imageCtrl.text,
-                        )
-                      : productProvider.addProduct(
-                          Product(
-                            id: widget.productId ?? DateTime.now().toString(),
-                            title: titleCtrl.text,
-                            description: descCtrl.text,
-                            price: double.parse(priceCtrl.text),
-                            imageUrl: imageCtrl.text,
-                          ),
-                        );
-
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  widget.productId == null ? 'Add Product' : 'Apply Changes',
-                ),
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -191,21 +218,30 @@ class CustomTextFormField extends StatelessWidget {
   final String labelText;
   final Widget? suffixIcon;
   final bool withImagePreview;
+  final void Function(String)? onFieldSubmitted;
+  final FocusNode? focusNode;
+  final int maxLines;
 
-  const CustomTextFormField(
-      {Key? key,
-      required this.controller,
-      required this.labelText,
-      this.withImagePreview = false,
-      this.suffixIcon})
-      : super(key: key);
+  const CustomTextFormField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.withImagePreview = false,
+    this.suffixIcon,
+    this.onFieldSubmitted,
+    this.focusNode,
+    this.maxLines = 1,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 10),
       child: TextFormField(
+        maxLines: maxLines,
+        focusNode: focusNode,
         controller: controller,
+        onFieldSubmitted: onFieldSubmitted,
         decoration: InputDecoration(
           labelText: labelText,
           suffixIcon: suffixIcon,
