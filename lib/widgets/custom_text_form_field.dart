67ffbdf42b58722ca8fa7c3ade6b621ffbb3aaae
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-29 11:25:59 +0700

    Create custom_text_form_field.dart

---
+++ lib/widgets/custom_text_form_field.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String labelText;
+  final String? initialValue;
+  final Widget? suffixIcon;
+  final bool withImagePreview;
+  final void Function(String)? onFieldSubmitted;
+  final String? Function(String?)? validator;
+  final FocusNode? focusNode;
+  final int maxLines;
+  final void Function(String?)? onSaved;
+
+  const CustomTextFormField({
+    Key? key,
+    required this.labelText,
+    this.controller,
+    this.withImagePreview = false,
+    this.suffixIcon,
+    this.onFieldSubmitted,
+    this.onSaved,
+    this.focusNode,
+    this.maxLines = 1,
+    this.initialValue,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: TextFormField(
+        validator: validator,
+        initialValue: initialValue,
+        onSaved: onSaved,
+        maxLines: maxLines,
+        focusNode: focusNode,
+        controller: controller,
+        onFieldSubmitted: onFieldSubmitted,
+        decoration: InputDecoration(
+          labelText: labelText,
+          suffixIcon: suffixIcon,
+          border: OutlineInputBorder(
+            borderRadius: withImagePreview
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  )
+                : BorderRadius.circular(10),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: withImagePreview
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  )
+                : BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
