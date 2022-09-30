752c08dd61e29c27c56521c0b03e78ddcfa95eec
naufaladikbds <naufal.adi@kbdsi.com>
2022-09-30 16:57:04 +0700

    Integrate fetchProducts, editProduct, and removeProduct to firebase database

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -2,44 +2,48 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:shop_shop/models/http_exception.dart';
 import 'package:shop_shop/models/product.dart';
 
 import 'package:http/http.dart' as http;
 
 class ProductsProvider with ChangeNotifier {
+  final String hostUrl =
+      'https://shop-shop-flutter-default-rtdb.asia-southeast1.firebasedatabase.app';
+
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
+    // Product(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageUrl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
   ];
 
   // bool isShowFavoritesOnly = false;
@@ -49,6 +53,42 @@ class ProductsProvider with ChangeNotifier {
     //   return _items.where((element) => element.isFavorite).toList();
     // }
     return [..._items];
+  }
+
+  Future<void> fetchProducts() async {
+    final Uri parsedUrl = Uri.parse('$hostUrl/products.json');
+
+    try {
+      final response = await http.get(parsedUrl);
+
+      final Map? responseBody = json.decode(response.body);
+
+      List<Product> productList = [];
+
+      responseBody?.forEach((key, value) {
+        productList.add(
+          Product(
+            id: key,
+            title: value['title'],
+            description: value['description'],
+            price: value['price'],
+            imageUrl: value['imageUrl'],
+          ),
+        );
+      });
+
+      return Future.delayed(Duration(milliseconds: 400), () {
+        _items = productList;
+        notifyListeners();
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void clearProducts() {
+    _items.clear();
+    notifyListeners();
   }
 
   List<Product> get favoriteItems {
@@ -67,8 +107,7 @@ class ProductsProvider with ChangeNotifier {
     required String imageUrl,
     required bool isFavorite,
   }) async {
-    final Uri parsedUrl = Uri.parse(
-        'https://shop-shop-flutter-default-rtdb.asia-southeast1.firebasedatabase.app/products.json');
+    final Uri parsedUrl = Uri.parse('$hostUrl/products.json');
 
     Map<String, dynamic> requestBody = {
       'title': title,
@@ -98,25 +137,46 @@ class ProductsProvider with ChangeNotifier {
     });
   }
 
-  void editProduct(Product product) {
-    int existingItemIndex =
-        _items.indexWhere((element) => element.id == product.id);
-    _items.removeWhere((element) => element.id == product.id);
-    _items.insert(
-      existingItemIndex,
-      Product(
-        id: product.id,
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl,
-      ),
-    );
+  Future<void> editProduct(Product product) async {
+    Uri parsedUrl = Uri.parse('$hostUrl/products/${product.id}.json');
+
+    final requestBody = {
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite': product.isFavorite,
+    };
+
+    await http.patch(parsedUrl, body: json.encode(requestBody));
+    // final responseBody = json.decode(response.body);
+
+    int existingItemIndex = _items.indexWhere((e) => e.id == product.id);
+    _items.removeWhere((e) => e.id == product.id);
+    _items.insert(existingItemIndex, product);
     notifyListeners();
   }
 
-  void removeProduct(String productId) {
-    _items.removeWhere((element) => element.id == productId);
+  Future<void> removeProduct(String productId) async {
+    Uri parsedUrl = Uri.parse('$hostUrl/products/${productId}.json');
+    final targetProductIndex =
+        _items.indexWhere((element) => element.id == productId);
+
+    try {
+      final response = await http.delete(parsedUrl);
+      if (response.statusCode >= 400) {
+        print(1);
+        throw HttpException(message: 'Something happened boiiii');
+      } else {
+        _items.removeAt(targetProductIndex);
+      }
+    } catch (e) {
+      print(2);
+      print(e.toString());
+      print(3);
+      rethrow;
+    }
+
     notifyListeners();
   }
 
