9d1d84528187a698b0a96dba652fe283cd1531b7
naufaladikbds <naufal.adi@kbdsi.com>
2022-10-06 15:04:40 +0700

    Add userId and token to product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -6,45 +6,55 @@ import 'package:shop_shop/models/http_exception.dart';
 
 class Product with ChangeNotifier {
   final String id;
+  final String userId;
   final String title;
   final String description;
   final double price;
   final String imageUrl;
+  final String? token;
   bool isFavorite;
 
   Product({
     required this.id,
+    required this.userId,
     required this.title,
     required this.description,
     required this.price,
     required this.imageUrl,
-    required this.isFavorite,
+    this.isFavorite = false,
+    this.token,
   });
 
   final String hostUrl =
       'https://shop-shop-flutter-default-rtdb.asia-southeast1.firebasedatabase.app';
 
-  Future<void> toggleFavorite() async {
+  Future<void> toggleFavorite({required String userId}) async {
     isFavorite = !isFavorite;
     notifyListeners();
 
-    Uri parsedUrl = Uri.parse('$hostUrl/products/$id.json');
-    final req = {'isFavorite': isFavorite};
+    Uri parsedUrl =
+        Uri.parse('$hostUrl/userFavorites/$userId/$id.json?auth=${token}');
+    final req = isFavorite;
 
     try {
-      final response = await http.patch(
+      final response = await http.put(
         parsedUrl,
         body: json.encode(req),
       );
 
-      final responseBody = jsonDecode(response.body);
+      final bool responseBody = jsonDecode(response.body);
+
+      // if (responseBody['error'] != null) {
+      //   throw HttpException(
+      //       message: '${response.statusCode}: ${responseBody['error']}');
+      // }
 
       print(responseBody);
       print(response.statusCode);
     } catch (e) {
       isFavorite = !isFavorite;
       notifyListeners();
-      throw HttpException(message: 'Caught Error: $e');
+      throw HttpException(message: '$e');
     }
   }
 }
